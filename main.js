27e43da7312326c7613b3f0a3a9e875784ce7988
Mitch Henline <rmhenline@gmail.com>
2022-11-02 15:17:49 -0600

    another

---
+++ main.js
@@ -0,0 +1,32 @@
+const plantBtn = document.getElementById("plantButton")
+const plantInfoBox = document.getElementById("plant-info-box")
+
+const getPlant = () => {
+    axios.get("http://localhost:4545/api/plant/")
+        .then(res => {
+            const data = res.data;
+            createPlantCard(data)
+    })
+};
+
+
+
+
+function createPlantCard(plant){
+    const plantCard = document.createElement('div')
+    plantCard.classList.add('plant-card')
+
+    plantCard.innerHTML = `<img alt="plant picture" src=${plant.imageURL} class="plant-picture"/>
+    <p class ="plant-title">${plant.title}</p>
+    <p class ="scientific-name">${plant.sName}</p>
+    <p class ="plant-type">Type: ${plant.type}</p>
+    </div>`
+
+    plantInfoBox.appendChild(plantCard)
+};
+
+plantBtn.addEventListener('click', getPlant)
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+  })
--- server/server.js
+++ server/server.js
@@ -16,3 +16,4 @@ app.get("/", home)
 const { PORT } = process.env
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+
